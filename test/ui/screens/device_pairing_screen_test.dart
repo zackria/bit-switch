73c466dc63544fc17f155c205ebcb795da88020c
Zack Dawood <zackria@gmail.com>
2026-09-13 12:47:24 -0400

    Update device_pairing_screen_test.dart

--- test/ui/screens/device_pairing_screen_test.dart
+++ test/ui/screens/device_pairing_screen_test.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
